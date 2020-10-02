f850526e6b40e3161c9f00adce2452eb78a88589
SilviuVladut <silviu.vladut18@gmail.com>
2020-10-02 09:02:29 +0300

    broken our hpg into smaller comp, finish styling our menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return <div>
@@ -10,3 +10,4 @@ function App() {
 }
 
 export default App;
+
